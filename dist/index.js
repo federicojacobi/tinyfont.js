3450eef1c5bc441757dd8b9a2cef4d9416ddc9de
Federico Jacobi <fjacobi@bhmdigital.com>
2025-06-16 13:45:32 -0400

    A few bytes larger renderer, but vastly more efficient

--- dist/index.js
+++ dist/index.js
@@ -13,20 +13,34 @@ const initFont = ({ height=DEFAULT_CHAR_HEIGHT, ...chars }={}, ctx) => {
         return
     }
 
-    return (string, x=0, y=0, size=24, color=DEFAULT_COLOR) => {
+    return ( string, x = 0, y = 0, size = 24, color = DEFAULT_COLOR ) => {
         const pixelSize = size / height;
-        const renderChar = (charX, char) => {
-            const fontCode = chars[char.charCodeAt()] || '';
+        const renderChar = ( charX, char ) => {
+            const fontCode = chars[ char.charCodeAt() ] || '';
             let bin = Number.isInteger( fontCode ) ? fontCode : fontCode.codePointAt();
             const binary = ( bin || 0 ).toString( 2 );
             const width = Math.ceil( binary.length / height );
+            const mask = ( 1 << height ) - 1;
 
             ctx.fillStyle = color;
             for ( let col = width; col > 0; col-- ) {
-                for ( let row = height; row > 0 && bin > 0; row-- ) {
-                    bin & 1 && ctx.fillRect( x + charX + col * pixelSize, y + row * pixelSize, pixelSize, pixelSize );
-                    bin >>= 1;
+                let rowPos = height - 1;
+                for ( let row = bin & mask; row > 0; row >>= 1 ) {
+                    if ( row & 1 ) {
+                        let y1 = y + rowPos * pixelSize, h = pixelSize;
+
+                        while ( ( row >> 1 ) & 1 ) {
+                            y1 -= pixelSize;
+                            h += pixelSize;
+                            row >>= 1;
+                            rowPos--;
+                        }
+
+                        ctx.fillRect( x + charX + col * pixelSize, y1, pixelSize, h );
+                    }
+                    rowPos--;
                 }
+                bin >>= height;
             }
 
             return charX + ( width + 1 ) * pixelSize;
--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ export const initFont = ({ height=DEFAULT_CHAR_HEIGHT, ...chars }={}, ctx) => {
         return
     }
 
-    return (string, x=0, y=0, size=24, color=DEFAULT_COLOR) => {
+    return ( string, x = 0, y = 0, size = 24, color = DEFAULT_COLOR ) => {
         const pixelSize = size / height;
-        const renderChar = (charX, char) => {
-            const fontCode = chars[char.charCodeAt()] || '';
+        const renderChar = ( charX, char ) => {
+            const fontCode = chars[ char.charCodeAt() ] || '';
             let bin = Number.isInteger( fontCode ) ? fontCode : fontCode.codePointAt();
             const binary = ( bin || 0 ).toString( 2 );
             const width = Math.ceil( binary.length / height );
@@ -22,9 +22,20 @@ export const initFont = ({ height=DEFAULT_CHAR_HEIGHT, ...chars }={}, ctx) => {
 
             ctx.fillStyle = color;
             for ( let col = width; col > 0; col-- ) {
-                let rowPos = height;
+                let rowPos = height - 1;
                 for ( let row = bin & mask; row > 0; row >>= 1 ) {
-                    row & 1 && ctx.fillRect( x + charX + col * pixelSize, y + rowPos * pixelSize, pixelSize, pixelSize );
+                    if ( row & 1 ) {
+                        let y1 = y + rowPos * pixelSize, h = pixelSize;
+
+                        while ( ( row >> 1 ) & 1 ) {
+                            y1 -= pixelSize;
+                            h += pixelSize;
+                            row >>= 1;
+                            rowPos--;
+                        }
+
+                        ctx.fillRect( x + charX + col * pixelSize, y1, pixelSize, h );
+                    }
                     rowPos--;
                 }
                 bin >>= height;
