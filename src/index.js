b13826a67e397dbaa16e53cf949749e0a5679a35
Federico Jacobi <fjacobi@bhmdigital.com>
2025-06-17 21:56:40 -0400

    Save decoded font in a Typed Array for better performance

--- src/index.js
+++ src/index.js
@@ -11,16 +11,33 @@ export const initFont = ({ height=DEFAULT_CHAR_HEIGHT, ...chars }={}, ctx) => {
         return
     }
 
+    const keys = Object.keys( chars ).map( i => parseInt( i ) );
+    const len = Math.max( ... keys ) + 1;
+    const widths = new Uint8Array( len );
+    const bins = new Uint32Array( len );
+
+    // Decode the binary values for each char ahead of time. This is slow-ish.
+    keys.forEach( ( key ) => {
+        const char = chars[ key ];
+        if ( char != undefined ) {
+            const bin = Number.isInteger( char ) ? char : char.codePointAt();
+            const binary = bin.toString( 2 );
+            const width = Math.ceil( binary.length / height );
+            bins[ key ] = bin;
+            widths[ key ] = width;
+        }
+    } );
+
+    const mask = ( 1 << height ) - 1;
+
     return ( string, x = 0, y = 0, size = 24, color = DEFAULT_COLOR ) => {
         const pixelSize = size / height;
-        const renderChar = ( charX, char ) => {
-            const fontCode = chars[ char.charCodeAt() ] || '';
-            let bin = Number.isInteger( fontCode ) ? fontCode : fontCode.codePointAt();
-            const binary = ( bin || 0 ).toString( 2 );
-            const width = Math.ceil( binary.length / height );
-            const mask = ( 1 << height ) - 1;
+        ctx.fillStyle = color;
+        for ( let i = 0, pad = 0; i < string.length; i++ ) {
+            let char = bins[ string.charCodeAt( i ) ];
+            let bin = char ? bins[ string.charCodeAt( i ) ] : 0;
+            const width = char ? widths[ string.charCodeAt( i ) ] : 0;
 
-            ctx.fillStyle = color;
             for ( let col = width; col > 0; col-- ) {
                 let rowPos = height - 1;
                 for ( let row = bin & mask; row > 0; row >>= 1 ) {
@@ -36,17 +53,14 @@ export const initFont = ({ height=DEFAULT_CHAR_HEIGHT, ...chars }={}, ctx) => {
                             rowPos--;
                         }
 
-                        ctx.fillRect( x + charX + col * pixelSize, y1, pixelSize, h );
+                        ctx.fillRect( x + pad + col * pixelSize, y1, pixelSize, h );
                     }
                     rowPos--;
                 }
                 bin >>= height;
             }
 
-            return charX + ( width + 1 ) * pixelSize;
+            pad += ( width + 1 ) * pixelSize;
         };
-
-        console.debug('Rendering string', string);
-        [...string].reduce(renderChar, 0);
     };
 };
